7385214c57c46bd935f6fdb7466199ab95521810
lch-coder <lch-coder@users.noreply.github.com>
2022-09-09 18:38:49 +0000

    deploy: 9bbeb3fb7386d73bd7624f91e76796babefab46c

--- assets/javascript.be160cbb.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript.f8b789f9.js";import"./editor.b2a6d6b6.js";import"./index.dd05b3c0.js";import"./el-button.e768163f.js";import"./plugin-vue_export-helper.f63bf49c.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.34.0(9d278685b078158491964f8fd7ac9628fffa0f30)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var n=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{n as conf,l as language};
