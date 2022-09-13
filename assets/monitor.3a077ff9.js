a9578b08dd63ff04c59756f4892326bb79090be1
lch-coder <lch-coder@users.noreply.github.com>
2022-09-13 16:33:45 +0000

    deploy: a30d2ab0915f7adae6c18f730e8f1f3a184d90a6

---
+++ assets/monitor.3a077ff9.js
@@ -0,0 +1,1 @@
+import{d as o,o as t,g as n,a7 as a,j as r}from"./index.12772b39.js";const _=o({__name:"monitor",setup(s){const e=Date.now();return(c,i)=>(t(),n("div",null,"Monitor - "+a(r(e)),1))}});export{_ as default};
