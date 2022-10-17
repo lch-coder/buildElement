b6aa3cfa1c70bbcc8233a2d959af4b752a174e41
lch-coder <lch-coder@users.noreply.github.com>
2022-10-17 13:51:46 +0000

    deploy: 01eb095be6f4dd7d56104ecae0186d6a0335159c

---
+++ assets/redirect.be8e3109.js
@@ -0,0 +1,1 @@
+import{d as o,aI as u,h as n,l as s,aJ as l}from"./index.f820dae8.js";const d=o({name:"redirect",setup(){const t=l(),r=s();return u(()=>{let{fullPath:e,query:a}=t;e=e.replace("/redirect",""),r.replace({path:e,query:a})}),()=>n("div",null,null)}});export{d as default};
