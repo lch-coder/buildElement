645f8d34f3aae656651fad3d5bf39697f43db8bb
lch <1660396419@qq.com>
2023-03-05 13:58:31 +0800

    github打包配置环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { visualizer } from 'rollup-plugin-visualizer'
@@ -15,76 +15,80 @@ import postcssPresetEnv from 'postcss-preset-env'
 import { formatDate } from './src/utils/time'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    terserOptions: {
-      compress: {
-        keep_infinity: true, // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
-        drop_console: true, // 生产环境去除 console
-        drop_debugger: true, // 生产环境去除 debugger
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  const { VITE_APP_BASE_URL: BASE_URL = '/' } = env
+  return defineConfig({
+    base: BASE_URL,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
       },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/styles/theme.scss" as *;`,
+    build: {
+      terserOptions: {
+        compress: {
+          keep_infinity: true, // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
+          drop_console: true, // 生产环境去除 console
+          drop_debugger: true, // 生产环境去除 debugger
+        },
       },
     },
-    postcss: {
-      plugins: [postcssPresetEnv()],
-    },
-  },
-  define: {
-    __PKG_TIME__: JSON.stringify(formatDate(new Date())),
-  },
-  plugins: [
-    vue(),
-    vueJsx(),
-    visualizer({
-      // open: true, //注意这里要设置为true，否则无效
-      gzipSize: true,
-      brotliSize: true,
-    }),
-    AutoImport({
-      imports: ['vue', 'vue-router', 'pinia', '@vueuse/core'],
-      dts: './src/auto-imports.d.ts',
-      eslintrc: {
-        enabled: false, // Default `false`
-        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
-        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/styles/theme.scss" as *;`,
+        },
       },
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      dirs: ['src/components'],
-      resolvers: [ElementPlusResolver()],
-      dts: 'src/components.d.ts',
-      include: [/\.vue$/, /\.vue\?vue/, /\.tsx$/, /\.md$/],
-    }),
-    // ElementPlus(),
-    Unocss(),
-    viteMockServe({
-      supportTs: false,
-      logger: false,
-      mockPath: './src/mock',
-      localEnabled: true, // 开发打包开关
-      prodEnabled: true, // 生产打包开关 // 这样可以控制关闭mock的时候不让mock打包到最终代码内
-      injectCode: ` import { setupProdMockServer } from './mockProdServer'; setupProdMockServer(); `,
-    }),
-    vueSetupExtend(),
-    eslintPlugin(),
-    webUpdateNotice({ logVersion: true, injectFileBase: '/' }),
-  ],
-  server: {
-    host: '0.0.0.0',
-    open: true,
-    port: 4646,
-  },
-})
+      postcss: {
+        plugins: [postcssPresetEnv()],
+      },
+    },
+    define: {
+      __PKG_TIME__: JSON.stringify(formatDate(new Date())),
+    },
+    plugins: [
+      vue(),
+      vueJsx(),
+      visualizer({
+        // open: true, //注意这里要设置为true，否则无效
+        gzipSize: true,
+        brotliSize: true,
+      }),
+      AutoImport({
+        imports: ['vue', 'vue-router', 'pinia', '@vueuse/core'],
+        dts: './src/auto-imports.d.ts',
+        eslintrc: {
+          enabled: false, // Default `false`
+          filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+          globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+        },
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        dirs: ['src/components'],
+        resolvers: [ElementPlusResolver()],
+        dts: 'src/components.d.ts',
+        include: [/\.vue$/, /\.vue\?vue/, /\.tsx$/, /\.md$/],
+      }),
+      // ElementPlus(),
+      Unocss(),
+      viteMockServe({
+        supportTs: false,
+        logger: false,
+        mockPath: './src/mock',
+        localEnabled: true, // 开发打包开关
+        prodEnabled: true, // 生产打包开关 // 这样可以控制关闭mock的时候不让mock打包到最终代码内
+        injectCode: ` import { setupProdMockServer } from './mockProdServer'; setupProdMockServer(); `,
+      }),
+      vueSetupExtend(),
+      eslintPlugin(),
+      webUpdateNotice({ logVersion: true, injectFileBase: BASE_URL }),
+    ],
+    server: {
+      host: '0.0.0.0',
+      open: true,
+      port: 4646,
+    },
+  })
+}
