bd23f9edfaafdf1ee08227af685f278070bbe28a
lch-coder <lch-coder@users.noreply.github.com>
2023-03-10 11:30:30 +0000

    deploy: 6a086c93723ec01c53e89af0e2c85ed1fd3ab027

---
+++ assets/monitor-cd9b4f7c.js
@@ -0,0 +1,1 @@
+import{d as s,G as a,H as m,m as t,i,j as n,B as o,l as d}from"./index-dd7d5872.js";const e={"m-2":""},r={"m-2":""},l=s({__name:"monitor",setup(s){const l=a(m(),"YYYY-MM-DD HH:mm:ss");return(s,a)=>(t(),i("div",null,[n("div",e,"当前时间："+o(d(l)),1),n("div",r,"打包时间："+o(d("2023-03-10 11:29:17")),1)]))}});export{l as default};
